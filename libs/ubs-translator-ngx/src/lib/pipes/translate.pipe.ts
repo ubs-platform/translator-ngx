49219d5a72abf162a543ddb5f7ce5ba24ad61da3
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-05-03 23:23:17 +0300

    accept translatortext

--- libs/ubs-translator-ngx/src/lib/pipes/translate.pipe.ts
+++ libs/ubs-translator-ngx/src/lib/pipes/translate.pipe.ts
@@ -1,7 +1,10 @@
 import { OnDestroy } from '@angular/core';
 import { ChangeDetectorRef, Pipe, PipeTransform } from '@angular/core';
 import { TranslatorRepositoryService } from '../service/translator-service.service';
-import { TranslationParameter } from '@ubs-platform/translator-core';
+import {
+  TranslationParameter,
+  TranslatorText,
+} from '@ubs-platform/translator-core';
 
 @Pipe({
   name: 'translate',
@@ -24,17 +27,28 @@ export class TranslatePipe implements PipeTransform, OnDestroy {
     this.disposeOld();
   }
 
-  transform(key: string, parameters?: TranslationParameter): string {
-    this.disposeOld();
-    this.subscription = this.translator
-      .getStringListenChanges({ key, parameters })
-      .subscribe((a) => {
-        if (this.translation != a) {
-          this.translation = a;
-          this.ref.markForCheck();
-        }
-      });
+  transform(key: TranslatorText, parameters?: TranslationParameter): string {
+    if (key != null) {
+      let _key, _parameters;
+      if (typeof key != 'string') {
+        _key = key.key;
+        _parameters = key.parameters;
+      } else {
+        _key = key;
+        _parameters = parameters;
+      }
+      this.disposeOld();
+      this.subscription = this.translator
+        .getStringListenChanges({ key: _key, parameters: _parameters })
+        .subscribe((a) => {
+          if (this.translation != a) {
+            this.translation = a;
+            this.ref.markForCheck();
+          }
+        });
 
-    return this.translation;
+      return this.translation;
+    }
+    return '';
   }
 }
